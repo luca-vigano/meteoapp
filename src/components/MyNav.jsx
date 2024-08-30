989ab864d6d14f7609e86d9693122deb85d89774
luca-vigano <luca.vigano@hotmail.com>
2024-08-30 10:43:01 +0200

    impostazione esame sett 10 meteo

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function MyNav() {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="#home">Libreria</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Browse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNav;
